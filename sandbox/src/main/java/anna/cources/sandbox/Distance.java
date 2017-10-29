b171014e17ce617af254a14ed7d1860ebf9fd662
Анна Храпова <anna_ermolina@mail.ru>
2017-10-29 21:23:23 +0300

    changes for task4

--- sandbox/src/main/java/anna/cources/sandbox/Distance.java
+++ sandbox/src/main/java/anna/cources/sandbox/Distance.java
@@ -3,16 +3,9 @@ package anna.cources.sandbox;
 public class Distance {
 
     public static void main (String[] args) {
-        Point p1 = new Point (5, 5);
-        Point p2 = new Point(10, 10);
+        Point p = new Point (0, 5, 10.5, 10);
 
-        System.out.println("Distance between ("+ p1.x +", "+ p1.y + ")  and ("+ p2.x +", "+ p2.y + ") is " + distance(p1, p2));
-    }
-
-    public static double distance (Point p1, Point p2) {
-
-        return Math.sqrt (Math.pow(p2.x-p1.x,2) + Math.pow(p2.y-p1.y,2));
-
+        System.out.println("Distance between ("+ p.x1 +", "+ p.y1 + ")  and ("+ p.x2 +", "+ p.y2 + ") is " + p.distance());
     }
 }
 
