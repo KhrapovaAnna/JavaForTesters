f31ed80d8dc3174139d68a8a6db47992e1cfc78e
Анна Храпова <anna_ermolina@mail.ru>
2017-10-29 21:10:35 +0300

    update Distance class to add variable into the message

--- sandbox/src/main/java/anna/cources/sandbox/Distance.java
+++ sandbox/src/main/java/anna/cources/sandbox/Distance.java
@@ -6,15 +6,12 @@ public class Distance {
         Point p1 = new Point (5, 5);
         Point p2 = new Point(10, 10);
 
-        System.out.println("Distance between two points is " + distance(p1, p2));
+        System.out.println("Distance between ("+ p1.x +", "+ p1.y + ")  and ("+ p2.x +", "+ p2.y + ") is " + distance(p1, p2));
     }
-
-
 
     public static double distance (Point p1, Point p2) {
 
-        double l;
-        return l = Math.sqrt ((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
+        return Math.sqrt (Math.pow(p2.x-p1.x,2) + Math.pow(p2.y-p1.y,2));
 
     }
 }
