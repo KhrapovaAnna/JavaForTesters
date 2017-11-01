b1ea8ecbf7e085c7117804c514ceb3ff65bafce3
Анна Храпова <anna_ermolina@mail.ru>
2017-11-01 23:23:49 +0300

    Correct method Point. Update tests

--- sandbox/src/main/java/anna/cources/sandbox/Distance.java
+++ sandbox/src/main/java/anna/cources/sandbox/Distance.java
@@ -3,9 +3,10 @@ package anna.cources.sandbox;
 public class Distance {
 
     public static void main (String[] args) {
-        Point p = new Point (0, 0, 0, 0);
+        Point p1 = new Point (0, 0);
+        Point p2 = new Point (10, 10);
 
-        System.out.println("Distance between ("+ p.x1 +", "+ p.y1 + ")  and ("+ p.x2 +", "+ p.y2 + ") is " + p.distance());
+        System.out.println("Distance between ("+ p1.x +", "+ p1.y + ")  and ("+ p2.x +", "+ p2.y + ") is " + p1.distance(p2));
     }
 }
 
--- sandbox/src/test/java/anna/cources/sandbox/PointTest.java
+++ sandbox/src/test/java/anna/cources/sandbox/PointTest.java
@@ -7,22 +7,26 @@ public class PointTest {
 
     @Test
     public void test1Length(){
-        Point p1 = new Point(0, 5, 10.5, 10);
-        Assert.assertEquals(p1.distance(), 12.0);
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point (10, 10);
+        Assert.assertEquals(p1.distance(p2), 14.0);
     }
     @Test
     public void test2Length() {
-        Point p1 = new Point (0,0,1,1);
-        Assert.assertEquals(p1.distance(), 1.0);
+        Point p1 = new Point (0,0);
+        Point p2 = new Point (1, 1);
+        Assert.assertEquals(p1.distance(p2), 1.0);
     }
     @Test
     public void test3Length(){
-        Point p1 = new Point (5,-5,33,16);
-        Assert.assertEquals(p1.distance(), 35.5);
+        Point p1 = new Point (5,-5);
+        Point p2 = new Point (33, 16);
+        Assert.assertEquals(p1.distance(p2), 35.00);
     }
     @Test
     public void test4Length(){
-        Point p1 = new Point (0,-0,0,0);
-        Assert.assertEquals(p1.distance(), 0.0);
+        Point p1 = new Point (0,-0);
+        Point p2 = new Point (0, 0);
+        Assert.assertEquals(p1.distance(p2), 0.0);
     }
 }
