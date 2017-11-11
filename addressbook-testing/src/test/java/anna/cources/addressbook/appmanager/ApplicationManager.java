9f0da4d4a2ca31e92fa56a823e06f6d4e541f9e0
Анна Храпова <anna_ermolina@mail.ru>
2017-11-12 02:03:59 +0300

    HelperBase created.

--- addressbook-testing/src/test/java/anna/cources/addressbook/appmanager/ApplicationManager.java
+++ addressbook-testing/src/test/java/anna/cources/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
 package anna.cources.addressbook.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
